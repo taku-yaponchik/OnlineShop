a5289fa223296ee9fb42429880669a937c3014e5
Tariel <taku-yaponchik@gmail.com>
2020-09-05 11:55:50 +0600

    added new file forms.py

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+# модель заказа для сохранения данныъ заказа
+from products.models import Product
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+    def __str__(self):
+        return 'Order №{}'.format(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='item', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_item', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
