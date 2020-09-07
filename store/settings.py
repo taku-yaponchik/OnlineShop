b3a6515a1ef52d7211b245264822de2143ca9a74
Tariel <taku-yaponchik@gmail.com>
2020-09-07 19:03:04 +0600

    allowed host

--- store/settings.py
+++ store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['secret-badlands-60391.herokuapp.com/products', '127.0.0.1']
+ALLOWED_HOSTS = ['secret-badlands-60391.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
